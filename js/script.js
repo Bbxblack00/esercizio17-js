71376e25f3888b5b16f27ac663ff49d6d26dfcc2
Benito <bbxblack@gmail.com>
2021-04-06 10:03:04 +0200

    esercizio live

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-function
+function testFun(x, y, ...rest) {
+
+  var myArr = [1, 2, 3, 4];
+
+  // for (var i = 0; i < myArr.length; i++) {
+  //
+  //   const elem = myArr[x];
+  //   console.log(elem);
+  // }
+
+  myArr.forEach((element, index) => console.log(index + ' ' + element));
+
+
+}
 
 function init() {
 
