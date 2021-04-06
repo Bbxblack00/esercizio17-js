9faa15cce89f75a3c764161dde3c90fa9d8aa54e
Benito <bbxblack@gmail.com>
2021-04-06 11:09:13 +0200

    esercizio live

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-function testFun(x, y, ...rest) {
+// function testFun(x, y, ...rest) {
 
-  var myArr = [1, 2, 3, 4];
+  // var myArr = [1, 2, 3, 4];
 
   // for (var i = 0; i < myArr.length; i++) {
   //
@@ -8,15 +8,24 @@ function testFun(x, y, ...rest) {
   //   console.log(elem);
   // }
 
-  myArr.forEach((element, index) => console.log(index + ' ' + element));
+  // myArr.forEach((element, index) => console.log(index + ' ' + element));
   // myArr.forEach(function(element) {console.log(element)};
 
 
-}
+// }
 
 function init() {
 
-  testFun();
+  // testFun();
+
+  new Vue({
+    data: {
+
+      'msg': 'hello world!'
+    }
+  });
+
+  $('#title').text('Ciao Mondo');
 
   // const myFun = (x, y, z) => x;
 
